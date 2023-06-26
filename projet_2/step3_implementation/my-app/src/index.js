a6d4a2b57f402c50a254abc2517c8959d7b681c1
Salina Bee <siriuspilas@gmail.com>
2023-06-25 21:18:20 -0400

    Testing React

--- projet_2/step3_implementation/my-app/src/index.js
+++ projet_2/step3_implementation/my-app/src/index.js
@@ -4,11 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const myFirstElement = <h1>Hello React!</h1>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  myFirstElement
 );
 
 // If you want to start measuring performance in your app, pass a function
